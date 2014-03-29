c2f8b1f33c28a644fb4d3c8656a3ff030c306b97
Yangtb <731111475@qq.com>
2014-03-29 15:22:43 +0800

    加入检测站站点坐标

--- SSB/SSB/CaculateDistance.h
+++ SSB/SSB/CaculateDistance.h
@@ -8,8 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Location : NSObject
+@property(nonatomic,strong)NSString* locationId;
+@property(nonatomic,assign)double latitude;
+@property(nonatomic,assign)double longitude;
+
+-(id)initWithLatitude:(double)latitude Longitude:(double)longitude;
+
+@end
+
 @interface CaculateDistance : NSObject
 
 +(CGPoint)caculatePointwith:(double)sWeidu sJingdu:(double)sJingdu dWeidu:(double)dWeidu
                      dJingdu:(double)dJingdu rect:(CGRect)viewRect;
+
++(NSArray*)shuikuLocations;
+
 @end
--- SSB/SSB/CaculateDistance.m
+++ SSB/SSB/CaculateDistance.m
@@ -6,8 +6,24 @@
 //  Copyright (c) 2014年 YTB. All rights reserved.
 //
 
-//#import <math.h>
+#import <CoreLocation/CoreLocation.h>
 #import "CaculateDistance.h"
+
+
+@implementation Location
+@synthesize latitude,longitude,locationId;
+
+-(id)initWithLatitude:(double)lat Longitude:(double)longt
+{
+    self = [super init];
+    if (self) {
+        longitude = longt;
+        latitude = lat;
+    }
+    return self;
+}
+
+@end
 
 #define EARTH_RADIUS 6378.137
 
@@ -96,4 +112,156 @@ dJingdu:(double)dJingdu rect:(CGRect)viewRect
     return caculate(sWeidu, sJingdu, dWeidu,
                     dJingdu, viewRect);
 }
+
++(NSArray*)shuikuLocations
+{
+    NSMutableArray * locations = [[NSMutableArray alloc] init];
+    Location *loc1 = [[Location alloc] initWithLatitude:du2Decimal(@"32.32.13") Longitude:du2Decimal(@"111.30.40")];
+    loc1.locationId = @"61940205";
+    [locations addObject:loc1];
+
+    Location *loc2 = [[Location alloc] initWithLatitude:du2Decimal(@"32.21.38")
+                                     Longitude:du2Decimal(@"110.53.18")];
+    loc2.locationId = @"61907600";
+    [locations addObject:loc2];
+    
+    Location *loc3 = [[Location alloc] initWithLatitude:du2Decimal(@"32.44.34")
+                                     Longitude:du2Decimal(@"111.22.56")];
+    loc3.locationId = @"62039130";
+    [locations addObject:loc3];
+
+    Location *loc4 = [[Location alloc] initWithLatitude:du2Decimal(@"32.22.54")
+                                     Longitude:du2Decimal(@"111.09.39")];
+    loc4.locationId = @"61907900";
+    [locations addObject:loc4];
+    
+    Location *loc5 = [[Location alloc] initWithLatitude:du2Decimal(@"32.33.17")
+                                     Longitude:du2Decimal(@"111.32.16")];
+    loc5.locationId = @"61940210";
+    [locations addObject:loc5];
+    
+    Location *loc6 = [[Location alloc] initWithLatitude:du2Decimal(@"32.48.29")
+                                     Longitude:du2Decimal(@"111.11.22")];
+    loc6.locationId = @"61907300";
+    [locations addObject:loc6];
+    
+    Location *loc7 = [[Location alloc] initWithLatitude:du2Decimal(@"32.19.06")
+                                     Longitude:du2Decimal(@"110.59.07")];
+    loc7.locationId = @"61907810";
+    [locations addObject:loc7];
+    
+    Location *loc8 = [[Location alloc] initWithLatitude:du2Decimal(@"32.37.59")
+                                     Longitude:du2Decimal(@"110.58.44")];
+    loc8.locationId = @"61906860";
+    [locations addObject:loc8];
+    
+    Location *loc9 = [[Location alloc] initWithLatitude:du2Decimal(@"32.28.26")
+                                     Longitude:du2Decimal(@"111.26.17")];
+    loc9.locationId = @"61908110";
+    [locations addObject:loc9];
+    
+    Location *loc10 = [[Location alloc] initWithLatitude:du2Decimal(@"")
+                                                                  Longitude:du2Decimal(@"")];
+    loc10.locationId = @"61907800";
+    [locations addObject:loc10];
+    
+    Location *loc11 = [[Location alloc] initWithLatitude:du2Decimal(@"32.38.40")
+                                     Longitude:du2Decimal(@"111.02.42")];
+    loc11.locationId = @"61906870";
+    [locations addObject:loc11];
+    
+    Location *loc12 = [[Location alloc] initWithLatitude:du2Decimal(@"32.47.14")
+                                     Longitude:du2Decimal(@"111.16.16")];
+    loc12.locationId = @"61907200";
+    [locations addObject:loc12];
+    
+    Location *loc13 = [[Location alloc] initWithLatitude:du2Decimal(@"32.45.22")
+                                     Longitude:du2Decimal(@"111.22.59")];
+    loc13.locationId = @"62010401";
+    [locations addObject:loc13];
+    
+    Location *loc14 = [[Location alloc] initWithLatitude:du2Decimal(@"32.25.24")
+                                     Longitude:du2Decimal(@"111.21.37")];
+    loc14.locationId = @"61940060";
+    [locations addObject:loc14];
+    
+    Location *loc15 = [[Location alloc] initWithLatitude:du2Decimal(@"32.26.15")
+                                     Longitude:du2Decimal(@"110.54.42")];
+    loc15.locationId = @"61938900";
+    [locations addObject:loc15];
+    
+    Location *loc16 = [[Location alloc] initWithLatitude:du2Decimal(@"32.27.00")
+                                     Longitude:du2Decimal(@"111.13.24")];
+    loc16.locationId = @"61939740";
+    [locations addObject:loc16];
+    
+    Location *loc17 = [[Location alloc] initWithLatitude:du2Decimal(@"32.21.32")
+                                     Longitude:du2Decimal(@"111.09.39")];
+    loc17.locationId = @"61939860";
+    [locations addObject:loc17];
+    
+    Location *loc18 = [[Location alloc] initWithLatitude:du2Decimal(@"32.51.41")
+                                     Longitude:du2Decimal(@"111.10.06")];
+    loc18.locationId = @"61937750";
+    [locations addObject:loc18];
+    
+    Location *loc19 = [[Location alloc] initWithLatitude:du2Decimal(@"32.45.01")
+                                     Longitude:du2Decimal(@"111.10.57")];
+    loc19.locationId = @"61937245";
+    [locations addObject:loc19];
+    
+    Location *loc20 = [[Location alloc] initWithLatitude:du2Decimal(@"32.40.02")
+                                     Longitude:du2Decimal(@"111.17.38")];
+    loc20.locationId = @"61939720";
+    [locations addObject:loc20];
+    
+    Location *loc21 = [[Location alloc] initWithLatitude:du2Decimal(@"32.45.43")
+                                     Longitude:du2Decimal(@"111.15.29")];
+    loc21.locationId = @"61938350";
+    [locations addObject:loc21];
+    
+    Location *loc22 = [[Location alloc] initWithLatitude:du2Decimal(@"32.32.53")
+                                     Longitude:du2Decimal(@"110.56.42")];
+    loc22.locationId = @"61939240";
+    [locations addObject:loc22];
+    
+    Location *loc23 = [[Location alloc] initWithLatitude:du2Decimal(@"32.34.15")
+                                     Longitude:du2Decimal(@"111.00.48")];
+    loc23.locationId = @"61937700";
+    [locations addObject:loc23];
+    
+    Location *loc24 = [[Location alloc] initWithLatitude:du2Decimal(@"32.35.16")
+                                     Longitude:du2Decimal(@"111.15.23")];
+    loc24.locationId = @"61939730";
+    [locations addObject:loc24];
+    
+    Location *loc25 = [[Location alloc] initWithLatitude:du2Decimal(@"32.30.15")
+                                     Longitude:du2Decimal(@"111.28.48")];
+    loc25.locationId = @"61940260";
+    [locations addObject:loc25];
+    
+    Location *loc26 = [[Location alloc] initWithLatitude:du2Decimal(@"32.31.57")
+                                     Longitude:du2Decimal(@"111.19.19")];
+    loc26.locationId = @"61940100";
+    [locations addObject:loc26];
+    
+    Location *loc27 =[[Location alloc] initWithLatitude:du2Decimal(@"32.38.55")
+                                     Longitude:du2Decimal(@"111.23.34")];
+    loc27.locationId = @"61940110";
+    [locations addObject:loc27];
+    
+    Location *loc28 =  [[Location alloc] initWithLatitude:du2Decimal(@"32.39.28")
+                                     Longitude:du2Decimal(@"111.06.02")];
+    loc28.locationId = @"61938250";
+    [locations addObject:loc28];
+    
+    Location *loc29 = [[Location alloc] initWithLatitude:du2Decimal(@"32.47.10")
+                                     Longitude:du2Decimal(@"111.11.07")];
+    loc29.locationId = @"61937760";
+    [locations addObject:loc29];
+    
+
+    return locations;
+}
+
 @end
